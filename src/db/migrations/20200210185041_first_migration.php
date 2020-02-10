e4075b5c68e4e2c6ee0e17b0f4692e1d2f444fe1
douglas <douglas@convesweb.com.br>
2020-02-10 17:20:49 -0300

    Migrations Tests

---
+++ src/db/migrations/20200210185041_first_migration.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class FirstMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table("disciplinas");
+        $table->addColumn("nome","string",["limit" => 50]);
+        $table->addColumn("descricao","text");
+        $table->addColumn("criado_por","integer",["limit" => 10]);
+        $table->addColumn("data_criacao",'timestamp', ['default' => 'CURRENT_TIMESTAMP']);
+        $table->create();
+    }
+}
